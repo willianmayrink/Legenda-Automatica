058fe86a1b8a26be28523edd203d32846753eda2
willianmayrink <51210094+willianmayrink@users.noreply.github.com>
2022-01-22 18:27:53 -0300

    Create CriandoFiltros.py

---
+++ CriandoFiltros.py
@@ -0,0 +1,90 @@
+import clr
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+import System
+from System.Collections.Generic import *
+from System.Collections.Generic import List
+
+doc=DocumentManager.Instance.CurrentDBDocument
+app=DocumentManager.Instance.CurrentUIApplication.Application
+#First construct an ICollection of StructuralFraming and Structural Column Categories:
+
+
+
+
+
+desniveis=[]
+numberColors=[]
+
+Floors = FilteredElementCollector(doc,doc.ActiveView.Id).OfCategory(BuiltInCategory.OST_Floors).ToElements()
+desniveisFloors=[]
+for elem in Floors:
+	if(elem.GetParameters("Height Offset From Level")[0].AsDouble()*30.48 != 0):
+		desniveis.append((elem.GetParameters("Height Offset From Level")[0].AsDouble()*30.48,"LAJE"))
+	desniveisFloors.append(elem.GetParameters("Height Offset From Level")[0].AsDouble()*30.48)
+	
+Beams = FilteredElementCollector(doc,doc.ActiveView.Id).OfCategory(BuiltInCategory.OST_StructuralFraming).ToElements()
+desniveisBeams=[]
+for elem in Beams:
+	if(elem.GetParameters("z Offset Value")[0].AsDouble()*30.48!=0):
+		desniveis.append((elem.GetParameters("z Offset Value")[0].AsDouble()*30.48,"VIGA"))
+	desniveisBeams.append(elem.GetParameters("z Offset Value")[0].AsDouble()*30.48)
+
+try:
+	numberColors= desniveisFloors + desniveisBeams
+	numberColors = list(dict.fromkeys(numberColors))
+	numberColors.remove(0)
+except:
+	numberColors = list(dict.fromkeys(numberColors))
+
+desniveisScheduleBeams=[]
+for item in desniveisBeams:
+    if(item in desniveisFloors):
+        desniveisScheduleBeams.append(item)
+
+Colors=(Color(0,0,0),Color(0,0,255),Color(255,0,0),Color(0,255,0),Color(255,255,0))
+
+
+pattern = UnwrapElement(IN[1])
+
+def InsertFilter(view, Filter, color):
+ view.AddFilter(Filter.Id)
+ overrides = OverrideGraphicSettings()
+ overrides.SetCutForegroundPatternColor(color).SetCutForegroundPatternId(pattern.Id).SetSurfaceForegroundPatternColor(color).SetSurfaceForegroundPatternId(pattern.Id)
+ view.SetFilterOverrides(Filter.Id, overrides)
+ 
+ 
+def CreateFilter(valueDesnivel, category ):
+  
+  if(category == "VIGA"):
+  	TransactionManager.Instance.EnsureInTransaction(doc)
+  	parameter=ParameterValueProvider(ElementId(BuiltInParameter.Z_OFFSET_VALUE))
+  	rule=FilterDoubleRule(parameter,FilterNumericEquals(),valueDesnivel/30.48,0)
+	tipo=List[ElementId]()
+	tipo.Add(ElementId(BuiltInCategory.OST_StructuralFraming))
+  	filter=ParameterFilterElement.Create(doc,category+"  "+str(float(valueDesnivel)),tipo ,ElementParameterFilter(rule))
+  	TransactionManager.Instance.TransactionTaskDone()
+  elif(category == "LAJE"):
+  	TransactionManager.Instance.EnsureInTransaction(doc)
+  	parameter=ParameterValueProvider(ElementId(BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM))
+  	rule=FilterDoubleRule(parameter,FilterNumericEquals(),valueDesnivel/30.48,0)
+	tipo=List[ElementId]()
+	tipo.Add(ElementId(BuiltInCategory.OST_Floors))
+  	filter=ParameterFilterElement.Create(doc,category+"  "+str(float(valueDesnivel)),tipo ,ElementParameterFilter(rule))
+  	TransactionManager.Instance.TransactionTaskDone()
+  return filter
+  
+#FillPatternElement.Create(doc,FillPattern())
+
+
+for elem in desniveis:
+ x=CreateFilter(elem[0],elem[1])
+ color=Colors[numberColors.index(elem[0])]
+ InsertFilter(doc.ActiveView, x, color)
